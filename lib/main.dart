96562112300e456ec1f13c6de8ca520e5cb632a2
shaf1501 <shafin15-4903@diu.edu.bd>
2025-02-18 12:57:15 +0600

    last

--- lib/main.dart
+++ lib/main.dart
@@ -3,89 +3,75 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-/*
-overflow handling:
-1. Single Child Scroll View
-2. Expanded
-3. 
-*/
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI - Flutter",
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("MyFirstApp"),
-            centerTitle: true,
-            backgroundColor: const Color.fromARGB(255, 219, 25, 177),
-            elevation: 10.5,
-          ),
-          body: Container(
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(
-                Icons.star_border,
-                size: 50,
-                color: const Color.fromARGB(255, 167, 81, 81),
-              ),
-              Icon(
-                Icons.abc_sharp,
-                size: 50,
-                color: const Color.fromARGB(255, 29, 29, 186),
-              ),
-              Icon(
-                Icons.star_border,
-                size: 50,
-                color: const Color.fromARGB(255, 115, 198, 130),
-              ),
-              Icon(
-                Icons.abc_sharp,
-                size: 50,
-                color: const Color.fromARGB(255, 224, 244, 7),
-              ),
-              Icon(
-                Icons.star_border,
-                size: 50,
-                color: const Color.fromARGB(255, 228, 109, 177),
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.abc_sharp,
-                    size: 50,
-                    color: Colors.black,
+      title: 'Fancy Icon App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Shoe widget'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text(
+                  '50% OFF',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Icon(Icons.favorite),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Nike Air Shoes',
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text(
+                  '\$45,000',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  '\$55,000',
+                  style: TextStyle(
+                    fontSize: 16,
+                    decoration: TextDecoration.lineThrough,
                   ),
-                  Icon(
-                    Icons.star_border,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Icons.abc_sharp,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Icons.star_border,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Icons.abc_sharp,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
